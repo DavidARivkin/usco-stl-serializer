531eaf00f29768942cbc164f8457e600437ade48
Mark Moissette <kaosat.dev@gmail.com>
2014-02-18 15:20:25 +0100

    - lots of cleanup of serializer: binary output now works as well (in the browser)  * fixed vertex data offset issues for binary output  * cleaned up structure of serializer  * added specs for binary output of object hierarchy  * added /updated browserDemo (client side generation)  * updated expected files  * added browserify build to lib/STLSerializer.js

--- specs/STLSerializer.spec.js
+++ specs/STLSerializer.spec.js
@@ -5,33 +5,82 @@ fs = require("fs");
 describe("STL serializer tests", function() {
   var serializer = new STLSerializer();
   
+  it("can serialize to binary stl files", function() {
+    var object = new THREE.Mesh(new THREE.CubeGeometry(10,10,10),new THREE.MeshBasicMaterial);
+    
+    var rawObsGeneratedSTL = serializer.serialize(object, 'binary');
+    fs.writeFileSync("obs.stl",rawObsGeneratedSTL);
+
+    var obsGeneratedSTL = fs.readFileSync("obs.stl", "binary");
+    var expGeneratedSTL = fs.readFileSync("specs/data/cube_bin.stl", "binary");
+    expect(obsGeneratedSTL).toEqual(expGeneratedSTL);
+  });
+
+  it("can serialize an object hierarchy to binary stl files", function() {
+    var object = new THREE.Mesh(new THREE.CubeGeometry(10,10,10),new THREE.MeshBasicMaterial);
+    var subObject = new THREE.Mesh(new THREE.SphereGeometry(10,10,10),new THREE.MeshBasicMaterial);
+    object.add( subObject );
+
+    var obsGeneratedSTL = serializer.serialize(object, 'binary');
+    var expGeneratedSTL = fs.readFileSync("specs/data/hierarchy_bin.stl", "binary")
+    expect(obsGeneratedSTL).toEqual(expGeneratedSTL);
+  });
+
+
   it("can serialize to ascii stl files", function() {
     var object = new THREE.Mesh(new THREE.CubeGeometry(10,10,10),new THREE.MeshBasicMaterial);
 
     var obsGeneratedSTL = serializer.serialize(object, 'ascii');
-    var expGeneratedStl = fs.readFileSync("specs/data/cube_ascii.stl", "utf8")
-    expect(obsGeneratedSTL).toEqual(expGeneratedStl);
-  });
-
-  it("can serialize to binary stl files", function() {
-    var object = new THREE.Mesh(new THREE.CubeGeometry(10,10,10),new THREE.MeshBasicMaterial);
-
-    var obsGeneratedSTL = serializer.serialize(object, 'binary');
-    fs.writeFileSync("bla.stl",obsGeneratedSTL)
-    var expGeneratedStl = fs.readFileSync("specs/data/cube_bin.stl", "binary");
-    expect(obsGeneratedSTL).toEqual("");
+    var expGeneratedSTL = fs.readFileSync("specs/data/cube_ascii.stl", "utf8")
+    expect(obsGeneratedSTL).toEqual(expGeneratedSTL);
   });
 
   it("can serialize an object hierarchy to ascii stl files", function() {
     var object = new THREE.Mesh(new THREE.CubeGeometry(10,10,10),new THREE.MeshBasicMaterial);
     var subObject = new THREE.Mesh(new THREE.SphereGeometry(10,10,10),new THREE.MeshBasicMaterial);
+    subObject.position.x = 20;
     object.add( subObject );
 
     var obsGeneratedSTL = serializer.serialize(object, 'ascii');
-    var expGeneratedStl = fs.readFileSync("specs/data/hierarchy_ascii.stl", "utf8")
-    expect(obsGeneratedSTL).toEqual(expGeneratedStl);
+    var expGeneratedSTL = fs.readFileSync("specs/data/hierarchy_ascii.stl", "utf8")
+    expect(obsGeneratedSTL).toEqual(expGeneratedSTL);
   });
 
-  
+
+  /*To be used to write out the data using buffers
+
+    /*var buffer = new Buffer();
+    for(var i =0;i<rawObsGeneratedSTL.length;i++)
+    {
+      console.log("rawObsGeneratedSTL",i);
+      if(rawObsGeneratedSTL[i] instanceof Float32Array)
+      {
+        console.log("Float32Array");
+        //buffer
+      }
+      else if(rawObsGeneratedSTL[i] instanceof Uint8Array)
+      {
+        console.log("Uint8Array");
+        buffer.writeUInt8(rawObsGeneratedSTL[i], 0);
+      }
+      else if(rawObsGeneratedSTL[i] instanceof  Uint16Array)
+      {
+        console.log("Uint16Array");
+        buffer.writeUInt8(rawObsGeneratedSTL[i], 0);
+      }
+      else if(rawObsGeneratedSTL[i] instanceof Uint32Array)
+      {
+        console.log("Uint32Array")
+      }
+    }*/
+    var size = 0;
+    for(var i = 0, len = parts.length; i < len; ++i) {
+        size += typeof parts[i] === 'string' ? Buffer.byteLength(parts[i]) :
+                str(parts[i]).indexOf('ArrayBuffer') > -1 ? parts[i].byteLength :
+                parts[i].buffer ? parts[i].buffer.byteLength :
+                parts[i].length
+      }
+
+  */
   
 });
